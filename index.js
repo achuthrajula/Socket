83b26cb745b33722d583a4bd16e9d9d4acde0951
achuthrajula <achuthrajula1999@gmail.com>
2020-03-03 14:49:46 +0530

    Minor Updates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
-var http = require('http').createServer(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -8,10 +9,10 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
     socket.on('chat message', function(msg) {
-        io.emit('message: ' + msg);
+        io.emit('chat message', msg);
     });
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+http.listen(port, function() {
+    console.log('listening on *:' + port);
 });
